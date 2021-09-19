e78c82e9d610165567b9e1b85db1d724871974e9
LouisAtVonage <louis.runcieman@vonage.com>
2021-09-19 14:34:06 +0100

    reordered main.py

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ from text_analysis.summarise_text_analysers import SummaryOfTextAnalysers
 from text_analysis.text_analyser import TextAnalyser
 
 
+def create_text_analysers_from_directory(directory):
+    valid_file_formats = ('.txt')
+    tokens_to_remmove = get_tokens_to_remove()
+
+    text_analysers = {}
+    for filename in os.listdir(directory):
+        if filename.endswith(valid_file_formats):
+            text_analysers[filename] = create_text_analyser_from_file(directory + filename, tokens_to_remmove)
+    return text_analysers
+
+
 def get_tokens_to_remove():
     stopwords = nltk.corpus.stopwords.words("english")
     tokens_to_remove = stopwords + get_uninteresting_words("uninteresting_words.csv")
@@ -18,15 +29,11 @@ def get_uninteresting_words(filename):
     return temp
 
 
-def create_text_analysers_from_directory(directory):
-    valid_file_formats = ('.txt')
-    tokens_to_remmove = get_tokens_to_remove()
-
-    text_analysers = {}
-    for filename in os.listdir(directory):
-        if filename.endswith(valid_file_formats):
-            text_analysers[filename] = create_text_analyser_from_file(directory + filename, tokens_to_remmove)
-    return text_analysers
+def file_import(filename):
+    f = open(filename, 'r')
+    raw = f.read()
+    raw = raw.rstrip('\n')
+    return raw
 
 
 def create_text_analyser_from_file(filename, tokens_to_remove):
@@ -42,13 +49,6 @@ def create_text_analyser_from_file(filename, tokens_to_remove):
     return text_analyser
 
 
-def file_import(filename):
-    f = open(filename, 'r')
-    raw = f.read()
-    raw = raw.rstrip('\n')
-    return raw
-
-
 if __name__ == '__main__':
 
     relative_directory_path = "test_docs/"
@@ -56,4 +56,3 @@ if __name__ == '__main__':
     summary_of_text_analysers = SummaryOfTextAnalysers(text_analysers)
     df = summary_of_text_analysers.create_dataframe()
     show(df[df["Total occurrences"] >= 20])
-    # show(df)
